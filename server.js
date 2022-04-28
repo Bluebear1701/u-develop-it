b0f219d9086b42396aa422bb298d40d87cfbaefa
Blue <97713796+Bluebear1701@users.noreply.github.com>
2022-04-27 22:50:31 -0500

    able to pull data from data base

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,7 +8,22 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your MySQL username,
+        user: 'root',
+        // Your MySQL password
+        password: '@Gypsy1701!mas',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
+db.query('SELECT * FROM candidates', (err, rows) => {
+    console.log(rows);
+});
 
 
 
@@ -15,8 +31,8 @@ app.use(express.json());
 // Default response for any other request (Not Found)
 app.use((req, res) => {
     res.status(404).end();
-  });
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-  });
+});
